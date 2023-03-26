90ceb39b44c18f546c8b52cd4ed488c0a6a67768
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-26 14:52:13 +0200

    1000-ig minden szám négyzete while ciklussal

---
+++ 017.2. negyzetezerigwhileciklussal/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _017._2.negyzetezerigwhileciklussal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i * i < 1000)
+
+            {
+                Console.WriteLine("A(z) " + i + " négyzete: " + i * i);
+                i++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
